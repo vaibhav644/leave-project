f3561348fbbdf75870e19d948dc681ee0883e2c3
vaibhav <vaibhavshukla596@gmail.com>
2022-05-25 11:32:42 +0530

    get employee

--- src/main/java/in/leave/springbootmongodb/controller/EmployeeController.java
+++ src/main/java/in/leave/springbootmongodb/controller/EmployeeController.java
@@ -1,10 +1,18 @@
 package in.leave.springbootmongodb.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import in.leave.springbootmongodb.model.Employee;
 
 @RestController
 @RequestMapping("/Employee")
 public class EmployeeController {
+	@GetMapping("/get")
+	public Employee getById(@PathVariable String id) {
+		return null;
+	}
 
 }
